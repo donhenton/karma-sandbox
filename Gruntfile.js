21df0b44523fb599998892fdc33abc2e7a675d77
d <dhenton@localhost.com>
2015-03-27 12:06:51 -0500

    set up

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,3 +13,32 @@ module.exports = function(grunt) {
   grunt.registerTask('runtest', ['karma']);
   grunt.loadNpmTasks('grunt-karma');
 };
+
+
+/*
+  
+  
+  'use strict';
+
+module.exports = function (grunt) {
+
+    grunt.initConfig({
+        karma: {
+            test: {
+                configFile: 'public_html/karma/conf/karma.conf.js',
+                 singleRun: true 
+            },
+            xtree: {
+                configFile: 'public_html/tree_tests/conf/xtree.karma.conf.js',
+                 singleRun: true 
+            },
+        },
+    });
+    grunt.registerTask('runtest', ['karma:test']);
+    grunt.registerTask('runxtree', ['karma:xtree']);
+    grunt.loadNpmTasks('grunt-karma');
+};
+  
+  
+  
+ */
