63f03bb05249796e22c9caad176f1806865c3830
d <dhenton@localhost.com>
2015-04-02 08:37:27 -0500

    starting on using jasmine

---
+++ public_html/jasmine/conf/jasmine.conf.js
@@ -0,0 +1,71 @@
+// Karma configuration
+// Generated on Tue Mar 24 2015 09:16:43 GMT-0500 (CDT)
+//http://karma-runner.github.io/0.10/config/configuration-file.html
+
+module.exports = function (config) {
+    config.set({
+        // base path that will be used to resolve all patterns (eg. files, exclude)
+        basePath: '../../..',
+        autoWatch: false,
+        // frameworks to use
+        // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+        frameworks: ['qunit'],
+        // list of files / patterns to load in the browser
+        files: [
+            'public_html/js/jquery/dist/jquery.js',
+            'public_html/js/jsxml.js',
+            'public_html/karma/tests/**_tests.js',
+            'public_html/qunit/transform/test_transform.xsl',
+            'public_html/karma/templates/a1.html',
+            'public_html/qunit/transform/test.xml'
+        ],
+        htmlReporter: {
+            outputFile: 'html_out/units.html' 
+ 
+        },
+        // preprocess matching files before serving them to the browser
+        // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+        preprocessors: {
+            '**/*.html': ['html2js'],
+            '**/*.xslt': ['html2js'],
+            '**/*.xml': ['html2js'],
+            '**/*.xsl': ['html2js'],
+            '**/*.js': ['coverage']
+        },
+        junitReporter: {
+            outputFile: 'junit/test-results.xml',
+            suite: 'bonzo'
+        },
+        coverageReporter: {
+            reporters: [{
+                    type: 'cobertura',
+                    dir: 'coverage/',
+                    subdir: 'chrome',
+                    file: 'chrome.xml'
+                }, {
+                    type: 'json',
+                    dir: 'coverage/',
+                    subdir: 'json',
+                    file: 'coverage.json'
+        }]
+        },
+        // test results reporter to use
+        // possible values: 'dots', 'progress'
+        // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+        reporters: ['progress', 'coverage', 'html', 'dots', 'junit'],
+        // web server port
+        port: 9876,
+        // enable / disable colors in the output (reporters and logs)
+        colors: true,
+        // level of logging
+        // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+        logLevel: config.LOG_DEBUG,
+        // start these browsers
+        // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+        //browsers: ['PhantomJS'],
+        browsers: ['Chrome'],
+        // Continuous Integration mode
+        // if true, Karma captures browsers, runs the tests and exits
+        singleRun: true
+    });
+};
