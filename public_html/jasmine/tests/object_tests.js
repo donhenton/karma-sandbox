70ca670005dff5fff15aa12b5924266efd359b25
d <dhenton@localhost.com>
2015-11-21 09:20:52 -0600

    added tests for prototypes

--- public_html/jasmine/tests/object_tests.js
+++ public_html/jasmine/tests/object_tests.js
@@ -1,60 +1,86 @@
 /* global expect, spyOn, NS */
 //http://jasmine.github.io/2.0/introduction.html
-describe("object_tests.js", function() {
-    
+describe("object_tests.js", function () {
+
     var myfunc = null;
-    beforeEach(function(){
-         myfunc = NS.myFunction;
-         spyOn(myfunc, 'init') ;
+    beforeEach(function () {
+        myfunc = NS.myFunction;
+        spyOn(myfunc, 'init');
     });
 
-    afterEach(function() {
-         myfunc.reset();
+    afterEach(function () {
+        myfunc.reset();
     });
-    
-     
-    it("should populate stuff during initialization", function(){
+
+
+    it("should populate stuff during initialization", function () {
         myfunc.init();
         //console.log(myfunc.toString()+" "+myfunc.stuff.length)
         expect(myfunc.stuff.length).toEqual(1);
         expect(myfunc.stuff[0]).toEqual('Testing');
     });
-  
-    it("should be able to initialize", function() {
+
+    it("should be able to initialize", function () {
         expect(myfunc.init).toBeDefined();
         myfunc.init();
         expect(myfunc.init).toHaveBeenCalled();
-       
+
     });
 
-    
+
 
     //ADDITIONAL TESTS TO WRITE FIRST:
-    describe("appending strings", function() {
-        it("should be able to append 2 strings", function() {
+    describe("appending strings", function () {
+        it("should be able to append 2 strings", function () {
             expect(myfunc.append).toBeDefined();
         });
-        it("should append 2 strings", function() {
-            expect(myfunc.append('hello ','world')).toEqual('hello world');
+        it("should append 2 strings", function () {
+            expect(myfunc.append('hello ', 'world')).toEqual('hello world');
         });
     });
-    
-    
-     describe("slice tests", function() {
-         
-         var initArray = [1,2,3,4];
-         var result = initArray.splice(1,1);
-        it("[1,2,3,4] split 1", function() {
-            expect(initArray).toEqual([1,3,4]);
+
+
+    describe("splice tests", function () {
+
+        var initArray = [1, 2, 3, 4];
+        var result = initArray.splice(1, 1);
+        it("[1,2,3,4] split 1", function () {
+            expect(initArray).toEqual([1, 3, 4]);
         });
-        
-        var kArray = [{"key":"alpha","value":34},{"key":"beta","value":79}];
-        kArray.splice(0,1);
-         it("object split", function() {
+
+        var kArray = [{"key": "alpha", "value": 34}, {"key": "beta", "value": 79}];
+        kArray.splice(0, 1);
+        it("object split", function () {
             expect(kArray.length).toEqual(1);
             expect(kArray[0].key).toEqual("beta");
         });
     });
-    
+
+    describe("prototype tests", function () {
+
+        var gamma = function ()
+        {
+
+        }
+
+        it("functions have prototypes", function () {
+            expect(typeof gamma.prototype).toEqual('object');
+
+        });
+
+        var Alpha = {};
+
+
+        it("objects dont have prototypes", function () {
+            expect(typeof Alpha.prototype).toEqual('undefined');
+
+        });
+
+    });
+
+
+
+
+
 });
 
